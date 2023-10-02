0be45edc3b18b2f8ab068f579740200c03deff87
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-10-02 21:25:30 +0700

    tailwind config and output

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#14b8a6',
+        secondary: '#64748b',
+        dark: '#002366',
+        gold: '#D0A84B',
+        whiteblue: '#CBD5E1',
+        darkestgreen: '#1C4144',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+      fontFamily: {
+        'Poppins': ['Poppins', 'san-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
