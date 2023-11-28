68e2f60877f39ec695a37efe8bd8eaa720283699
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-11-28 15:16:09 +0700

    video frame finish

--- public/js/script.js
+++ public/js/script.js
@@ -6,3 +6,25 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+// Play Pause Buttton
+var video = document.getElementById("myVideo");
+var playPauseBtn = document.getElementById("playPauseBtn");
+
+    // Event listener untuk memantau perubahan status video
+    video.addEventListener("play", function() {
+        playPauseBtn.style.display = "none"; 
+        // Menyembunyikan tombol saat video diputar
+    });
+
+    video.addEventListener("ended", function() {
+        playPauseBtn.style.display = "block"; // Menampilkan kembali tombol saat video selesai
+    });
+
+    playPauseBtn.addEventListener("click", function() {
+      if (video.paused) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
